8d43218d0e832d957cba8ccbe50db9fdd4aa0ed0
Jannik Meier <jannik-m-1@web.de>
2024-08-12 18:46:31 +0200

    added a general styling css file for font size and color

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $siteTitle }} - Jannik Meier</title>
 
+    <link href="{{ asset('css/generalStyling.css') }}" rel="stylesheet">
 </head>
 <body>
     
-    <x-navigation-bar/>
-
-    {{ $slot }}
-
+    <div class="siteWrapper">
+        <x-navigation-bar/>
+        {{ $slot }}
+    </div>
+    
+    
     <script src="{{ asset('js/navigationBar.js') }}"></script>
 </body>
 </html>
