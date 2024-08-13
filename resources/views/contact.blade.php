41af621ff87a6177d1653e27db92d40d8b6f5b79
Jannik Meier <jannik-m-1@web.de>
2024-08-13 12:29:40 +0200

    added text for contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,19 @@
-<x-layout>
-    Contact
-</x-layout>
+@extends('components.layout')
+
+@section('siteTitle', 'Contact')
+
+@section('contact')
+
+    <link href="{{ asset('css/contact.css') }}" rel="stylesheet">
+    
+    <h1>GET IN TOUCH</h1>
+    <div class="contactWrapper">
+        <div>
+            <b style="font-size: 1.6rem;color:#E5E5E5;">DON'T BE SHY!</b><br>
+            <p>Feel free to get in touch with me.<br> I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
+        </div>
+    </div>
+
+    @include('components.email-form')
+
+@endsection
