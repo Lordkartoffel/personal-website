e9451507a3ea06a91b36a281a9368655f3906bb7
Jannik Meier <jannik-m-1@web.de>
2024-08-23 15:58:07 +0200

    added div tags with id for navigation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,18 +10,30 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
 
     <div class="siteWrapper">
 
-        <x-navigation-bar/>
-        <x-home/>
-        <x-about/>
-        <x-ressources/>
-        <x-contact/>
+        <x-navigation-bar />
+
+        <div id="home">
+            <x-home />
+        </div>
+        <div id="about">
+            <x-about />
+        </div>
+        <div id="ressources">
+            <x-ressources />
+        </div>
+        <div id="contact">
+            <x-contact />
+        </div>
 
     </div>
-       
+
     <script src="{{ asset('js/navigationBar.js') }}"></script>
+    <script src="{{ asset('js/emailSendButton.js') }}"></script>
 </body>
+
 </html>
