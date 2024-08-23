5f9ad69dec5923e7dc77fd436bae3a5243e4b30e
Jannik Meier <jannik-m-1@web.de>
2024-08-24 00:02:01 +0200

    added margin for the form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,17 @@
 <div>
     <link href="{{ asset('css/contact.css') }}" rel="stylesheet">
-    
+
     <h1>GET IN TOUCH</h1>
     <div class="flex flex-row pt-10">
         <div class="basis-1/3">
             <b style="font-size: 1.6rem;color:#E5E5E5;">DON'T BE SHY!</b><br>
-            <p>Feel free to get in touch with me.<br> I am always open to discussing new projects,<br> creative ideas or opportunities to be part of your visions.</p>
+            <p>Feel free to get in touch with me.<br> I am always open to discussing new projects,<br> creative ideas or
+                opportunities to be part of your visions.</p>
         </div>
-        <div class="basis-2/3">
+        <div class="basis-2/3 ml-40">
             @include('components.email-form')
         </div>
     </div>
-    
-    
-</div>
-    
 
+
+</div>
