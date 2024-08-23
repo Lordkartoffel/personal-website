0cf846e569cf985f94699fec5093c1b0fd11b8c0
Jannik Meier <jannik-m-1@web.de>
2024-08-23 13:34:36 +0200

    deleted routes for single page

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 //Homepage
 Route::get('/', function () {
-    return view('home');
+    return view('index');
 });
 
-//Ressource Page
-Route::get('/ressources', function () {
-    return view('ressources');
-});
-
-//Contact Page
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-//About Page
-Route::get('/about', function () {
-    return view('about');
-});
