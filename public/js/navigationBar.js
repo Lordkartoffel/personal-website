6fbfc93acf3232241eb1782a1bc850fe74bba031
Jannik Meier <jannik-m-1@web.de>
2024-12-01 12:20:23 +0100

    removed old php code

--- public/js/navigationBar.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".menu-toggle").addEventListener("click", function () {
-    document.querySelector(".nav").classList.toggle("mobile-nav");
-    this.classList.toggle("is-active");
-});
