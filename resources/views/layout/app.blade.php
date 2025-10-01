af3cc4155c0c0cd4dcc75a6a80f7c1b4eb068ffb
Jannik Meier <jmeier@delta-software.com>
2025-10-01 12:13:00 +0200

    feat(layout): add HTML structure and implement head section

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'Jannik Meier')</title>
+    <link rel="icon" href="{{URL::asset('/images/JM_Logo.svg')}}" type="image/x-icon">
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    
+</body>
+</html>
