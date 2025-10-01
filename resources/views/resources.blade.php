0fe7c5614fed42a6984dd4c261c2eb84b0265a14
Jannik Meier <jmeier@delta-software.com>
2025-10-01 17:06:08 +0200

    feat: add header and div frame

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -3,4 +3,14 @@
 @section('title', 'Resources - Jannik Meier')
 
 @section('content')
+
+<div class="py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-4xl mx-auto">
+        <!-- Header -->
+        <div class="text-center mb-12">
+            <h1 class="text-4xl font-bold text-gray-900 dark:text-white mb-4">Resources</h1>
+            <p class="text-xl text-gray-600 dark:text-gray-300">Download my documents and certificates</p>
+        </div>
+    </div>
+</div>
 @endsection
